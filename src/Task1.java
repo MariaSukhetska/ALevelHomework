c9b8de09b0135df8f0ab59ea68d4492e3fa81f77
Maria Zhdanova <mariasuhxxx@gmail.com>
2023-10-07 00:21:50 +0300

    "added first task"

---
+++ src/Task1.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+
+public class Task1 {
+
+    public Boolean back(String A){
+
+        String B="";
+        String C=A.replace(" ","");
+        char[] mass =C.toCharArray();
+        char[] mass2=new char[mass.length];
+        for ( int i =mass.length-1; i>= 0;){
+            for(int j=0;j<mass2.length; j++){
+
+                mass2[j]=mass[i]; i--;
+
+                B += mass2[j];
+
+            }
+
+        }
+        System.out.println(B);
+        System.out.println(C);
+        return B.equalsIgnoreCase(C);
+
+    }
+
+    public static void main(String args[]){
+        Task1 C=new Task1();
+        System.out.println(C.back("A man a plan a canal Panama"));
+    }
+
+}
---
+++ src/Task2.java
@@ -0,0 +1,25 @@
+public class Task2 {
+    public static void main(String[] args) {
+        String str = "A man a plan a canal Panama";
+        String[] words = str.split("\\s+");
+        int count = 0;
+
+        for (String word : words) {
+            if (isValidWord(word)) {
+                count++;
+            }
+        }
+
+        System.out.println("Count: " + count);
+    }
+
+    private static boolean isValidWord(String word) {
+        char[] chars = word.toCharArray();
+        for (char c : chars) {
+            if (!Character.isAlphabetic(c)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
