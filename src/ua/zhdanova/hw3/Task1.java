9578b921bd4a7b733581facde2d9f12f38b0d675
Maria Zhdanova <mariasuhxxx@gmail.com>
2023-10-07 00:35:41 +0300

    "updated hw-3"

--- src/ua/zhdanova/hw3/Task1.java
+++ src/ua/zhdanova/hw3/Task1.java
@@ -1,4 +1,4 @@
-import java.util.Arrays;
+package ua.zhdanova.hw3;
 
 public class Task1 {
 
--- src/ua/zhdanova/hw3/Task2.java
+++ src/ua/zhdanova/hw3/Task2.java
@@ -1,3 +1,5 @@
+package ua.zhdanova.hw3;
+
 public class Task2 {
     public static void main(String[] args) {
         String str = "A man a plan a canal Panama";
