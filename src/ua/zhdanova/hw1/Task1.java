b3545fc43d2be39a068cc86230ccf50b71306ae6
Maria Zhdanova <mariasuhxxx@gmail.com>
2023-10-07 14:23:29 +0300

    updated HW1

--- src/ua/zhdanova/hw1/Task1.java
+++ src/ua/zhdanova/hw1/Task1.java
@@ -1,6 +1,6 @@
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
-public class Main {
+public class Task1 {
     public static void main(String[] args) {
         String name = "Maria";
         int age = 29;
