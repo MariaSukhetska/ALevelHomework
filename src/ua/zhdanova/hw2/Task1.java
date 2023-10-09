b91727f0fc996b17fb0d7d2e535781f77d870aef
Maria Zhdanova <147208260+MariaSukhetska@users.noreply.github.com>
2023-10-09 20:14:16 +0300

    "added method calculateTriangleSquare"

--- src/ua/zhdanova/hw2/Task1.java
+++ src/ua/zhdanova/hw2/Task1.java
@@ -5,7 +5,10 @@ public class Task1 {
         int x1 = 1, y1 = 5;
         int x2 = 3, y2 = 5;
         int x3 = 6, y3 = 7;
-        double square = 0.5 *  Math.abs ( ( x1 *( y2 -y3 ) + x2 *( y3 -y1 ) + x3 *( y1 -y2 ) ) );
-        System.out.println ( square );
+        double triangleSquare = calculateTriangleSquare ( x1, x2, x3, y1, y2, y3);
+        System.out.println (triangleSquare);
+    }
+    public static double calculateTriangleSquare (int x1, int x2, int x3,int y1, int y2,int y3)  {
+        return 0.5 *  Math.abs ( ( x1 *( y2 -y3 ) + x2 *( y3 -y1 ) + x3 *( y1 -y2 ) ) );
     }
 }
