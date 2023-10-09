e81b024f3d6bd1b2503466fd637b548a783a9acd
Maria Zhdanova <147208260+MariaSukhetska@users.noreply.github.com>
2023-10-09 22:53:16 +0300

    "added method findMin and findMinAbs"

--- src/ua/zhdanova/hw2/Task3.java
+++ src/ua/zhdanova/hw2/Task3.java
@@ -2,10 +2,28 @@ package ua.zhdanova.hw2;
 
 public class Task3 {
     public static void main(String[] args) {
-        int num1 = 8;
+        int num1 = -8;
         int num2 = 5;
         int num3 = 9;
-        System.out.println(Math.min(num1, Math.min(num2, num3)));
-        System.out.println(Math.min(num2%num1, Math.min(num3%num1, num3%num2)));
+        int minNumber = findMin(num1, num2, num3);
+        int minAbsNumber = findMinAbs(num1, num2, num3);
+        System.out.println(minNumber);
+        System.out.println(minAbsNumber);
+    }
+
+    public static int findMin(int num1, int num2, int num3) {
+        return Math.min(num1, Math.min(num2, num3));
+    }
+
+    public static int findMinAbs(int num1, int num2, int num3) {
+        int abs1 = Math.abs(num1);
+        int abs2 = Math.abs(num2);
+        int abs3 = Math.abs(num3);
+        int minAbs = Math.min(abs1, Math.min(abs2, abs3));
+        if (minAbs == abs1)
+            return abs1;
+        if (minAbs == abs2)
+            return abs2;
+        else return abs3;
     }
 }
--- src/ua/zhdanova/hw2/Task3_2.java
+++ src/ua/zhdanova/hw2/Task3_2.java
@@ -1,13 +1,23 @@
 package ua.zhdanova.hw2;
 
-public class Task3_2 {
+public class Task3_2  {
     public static void main(String[] args) {
-        int num1 = 8;
+        int num1 = -8;
         int num2 = 5;
         int num3 = 9;
-        int min1 = num1 < num2 ? (num1 < num3 ? num1 : num3) : ( num2 < num3 ? num2 : num3);
-        System.out.println(min1);
-        int min2 = (num2 % num1 < num3 % num1) ? (num2 % num1 < num3 % num2 ? num2 % num1 : num3 % num2) : (num3 % num1 < num3 % num2 ? num3 % num1 : num3 % num2);
-        System.out.println(min2);
+        int minNumber = findMin(num1, num2, num3);
+        int minAbsNumber = findMinAbs(num1, num2, num3);
+        System.out.println(minNumber);
+        System.out.println(minAbsNumber);
+    }
+
+    public static int findMin(int num1, int num2, int num3) {
+        return (num1 < num2 ? (num1 < num3 ? num1 : num3) : (num2 < num3 ? num2 : num3));
+    }
+    public static int findMinAbs(int num1, int num2, int num3) {
+        int abs1 = Math.abs(num1);
+        int abs2 = Math.abs(num2);
+        int abs3 = Math.abs(num3);
+        return (abs1 < abs2) ? (abs1 < abs3 ? abs1 : abs3) : (abs2 < abs3 ? abs2 : abs3);
     }
 }
