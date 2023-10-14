55799c0101c35cb917bf4c0501b542ff59070bc9
Maria Zhdanova <147208260+MariaSukhetska@users.noreply.github.com>
2023-10-14 11:01:40 +0300

    "added Task1_2"

--- src/ua/zhdanova/hw5/Task2.java
+++ src/ua/zhdanova/hw5/Task2.java
@@ -4,14 +4,11 @@ public class Task2 {
     public static void main(String[] args) {
         int[] array = {5, 3, 2, 1};
 
-        // А) Виводимо масив до операцій
         printArray("Original array:", array);
 
-        // Б) Результат перевірки виводимо в консоль в методі main().
         System.out.println(" Чи масив відсортований за спаданням ? " + isSortedDescending(array));
     }
 
-    // Виводимо масив в консоль
     public static void printArray(String message, int[] array) {
         System.out.print(message + " ");
         for (int i : array) {
@@ -20,7 +17,6 @@ public class Task2 {
         System.out.println();
     }
 
-    // Перевіряємо, чи впорядкований масив за спаданням
     public static boolean isSortedDescending(int[] array) {
         for (int i = 1; i < array.length; i++) {
             if (array[i] > array[i-1]) {
