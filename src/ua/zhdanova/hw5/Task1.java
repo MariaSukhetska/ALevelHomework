4507e762e994706a838a637897c3fa1b249de1cc
Maria Zhdanova <147208260+MariaSukhetska@users.noreply.github.com>
2023-10-15 21:20:18 +0300

    "updated Task1 (deleted copy of method)"

--- src/ua/zhdanova/hw5/Task1.java
+++ src/ua/zhdanova/hw5/Task1.java
@@ -4,8 +4,6 @@ public class Task1 {
     public static void main(String[] args) {
         int[][] array = new int[4][3];
         fillArray(array);
-        //printArray(array);
-        plusMinusString(array);
     }
 
     public static void fillArray(int[][] array) {
@@ -14,20 +12,9 @@ public class Task1 {
             for (int j = 0; j < array[i].length; j++) {
                 array[i][j] = k + 1;
                 k++;
-            }
-        }
-    }
-
-    //public static void printArray(int[][] array) {
-        //for (int i = 0; i < array.length; i++) {
-       // }
-   // }
-
-    public static void plusMinusString(int[][] array) {
-        for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array[i].length; j++) {
-                if (i % 2 != 0)
+                if (i % 2 != 0) {
                     array[i][j] = -1 * array[i][j];
+                }
                 System.out.print(array[i][j] + " ");
             }
             System.out.println();
