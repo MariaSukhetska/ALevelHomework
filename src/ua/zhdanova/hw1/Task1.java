f0647f658f1f8c78414ce5738fe24bc749000e06
Maria Zhdanova <147208260+MariaSukhetska@users.noreply.github.com>
2023-10-21 22:34:47 +0300

    "added folder to gitignore"

--- src/ua/zhdanova/hw1/Task1.java
+++ src/ua/zhdanova/hw1/Task1.java
@@ -1,10 +1,10 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+package ua.zhdanova.hw1;
+
 public class Task1 {
     public static void main(String[] args) {
         String name = "Maria";
         int age = 29;
         double weight = 47.5;
-            System.out.println(name + " " + age + " " + weight);
+        System.out.println(name + " " + age + " " + weight);
     }
 }
